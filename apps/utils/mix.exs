679f9e96d37a5017eec9d3e15335ea8110b6ee1b
Nicholas Henry <nicholas.henry@gmail.com>
2022-09-13 13:58:16 -0400

    Remove generated boilerplate form utils application

--- apps/utils/mix.exs
+++ apps/utils/mix.exs
@@ -16,7 +16,6 @@ defmodule Utils.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
@@ -28,10 +27,6 @@ defmodule Utils.MixProject do
   end
 
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
-    ]
+    []
   end
 end
--- apps/utils/test/utils_test.exs
+++ apps/utils/test/utils_test.exs
@@ -1,8 +1,3 @@
 defmodule UtilsTest do
   use ExUnit.Case
-  doctest Utils
-
-  test "greets the world" do
-    assert Utils.hello() == :world
-  end
 end

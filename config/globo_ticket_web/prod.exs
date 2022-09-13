de4792221cb29d5a80321c696574a20388cfe3c0
Nicholas Henry <nicholas.henry@gmail.com>
2022-09-13 15:06:07 -0400

    Enhance runtime configuration

--- config/globo_ticket_web/prod.exs
+++ config/globo_ticket_web/prod.exs
@@ -1,7 +1,6 @@
 import Config
 
 config :globo_ticket_web, GloboTicketWeb.Endpoint,
-  url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :info
--- config/runtime.exs
+++ config/runtime.exs
@@ -1,6 +1,8 @@
 import Config
 
-if config_env() == :prod do
+# CONFIG: globo_ticket
+
+if config_env() in [:prod] do
   database_url =
     System.get_env("DATABASE_URL") ||
       raise """
@@ -11,26 +13,53 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
   config :globo_ticket, GloboTicket.Repo,
-    # ssl: true,
+    ssl: System.get_env("DATABASE_SSL") != "false",
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    pool_size: String.to_integer(System.get_env("DATABASE_POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
+end
 
+# CONFIG: globo_ticket_web
+
+# NOTE: `PORT` cannot be renames as it is a default for platforms such as Heroku
+port = String.to_integer(System.get_env("PORT") || "4000")
+
+unless config_env() in [:test] do
+  endpoint =
+    cond do
+      url = System.get_env("ENDPOINT_URL") ->
+        URI.parse(url)
+
+      config_env() == :dev ->
+        URI.parse("http://localhost:#{port}")
+
+      true ->
+        raise """
+        environment variable ENDPOINT_URL is missing
+        For example: https://example.com
+        """
+    end
+
+  config :globo_ticket_web, GloboTicketWeb.Endpoint,
+    url: endpoint |> Map.to_list() |> Keyword.take([:scheme, :host, :port])
+end
+
+if config_env() in [:prod] do
   secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
+    System.get_env("ENDPOINT_SECRET_KEY_BASE") ||
       raise """
-      environment variable SECRET_KEY_BASE is missing.
+      environment variable ENDPOINT_SECRET_KEY_BASE is missing.
       You can generate one by calling: mix phx.gen.secret
       """
 
   config :globo_ticket_web, GloboTicketWeb.Endpoint,
     http: [
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
+      port: port
     ],
     secret_key_base: secret_key_base
 
-  if System.get_env("ENDPOINT_SERVER") do
+  if System.get_env("PHX_SERVER") do
     config :globo_ticket_web, GloboTicketWeb.Endpoint, server: true
   end
 end

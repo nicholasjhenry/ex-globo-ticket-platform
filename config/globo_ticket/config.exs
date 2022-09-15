02da97a38f2a91b5251e0771adf471759e309c1a
Nicholas Henry <nicholas.henry@gmail.com>
2022-09-15 09:52:55 -0400

    Add schema with timestamps customized

--- config/globo_ticket/config.exs
+++ config/globo_ticket/config.exs
@@ -3,6 +3,8 @@ import Config
 config :globo_ticket,
   ecto_repos: [GloboTicket.Repo]
 
+config :globo_ticket, GloboTicket.Repo, migration_timestamps: [type: :utc_datetime_usec]
+
 config :globo_ticket, GloboTicket.Mailer, adapter: Swoosh.Adapters.Local
 
 config :swoosh, :api_client, false

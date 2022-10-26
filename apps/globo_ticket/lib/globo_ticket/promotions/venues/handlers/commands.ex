c375534884c48a077a05ac8768bae6d845009524
Nicholas Henry <nicholas.henry@gmail.com>
2022-10-26 13:25:28 -0400

    refactor: Have the command handler align with acts

--- apps/globo_ticket/lib/globo_ticket/promotions/venues/handlers/commands.ex
+++ apps/globo_ticket/lib/globo_ticket/promotions/venues/handlers/commands.ex
@@ -7,18 +7,35 @@ defmodule GloboTicket.Promotions.Venues.Handlers.Commands do
   alias GloboTicket.Promotions.Venues.Records
 
   def save_venue(venue) do
-    record = %Records.Venue{uuid: venue.id}
-
-    with {:ok, _} <- save_entity_record(Repo, record),
-         record = get_venue!(venue.id),
-         {:ok, _} <- save_snapshot(Repo, venue, record, :description),
-         {:ok, _} <- save_snapshot(Repo, venue, record, :location, :location_last_updated_ticks),
-         {:ok, _} <- save_snapshot(Repo, venue, record, :time_zone, :time_zone_last_updated_ticks) do
+    with {:ok, record} <- save_entity(venue),
+         {:ok, _} <- save_description(venue, record),
+         {:ok, _} <- save_location(venue, record),
+         {:ok, _} <- save_time_zone(venue, record) do
       {:ok, venue}
     end
   end
 
-  defp get_venue!(venue_uuid) do
+  def save_entity(venue) do
+    record = %Records.Venue{uuid: venue.id}
+
+    with {:ok, _} <- save_entity_record(Repo, record) do
+      {:ok, get_record!(venue.id)}
+    end
+  end
+
+  defp save_description(venue, record) do
+    save_snapshot(Repo, venue, record, :description)
+  end
+
+  defp save_location(venue, record) do
+    save_snapshot(Repo, venue, record, :location, :location_last_updated_ticks)
+  end
+
+  defp save_time_zone(venue, record) do
+    save_snapshot(Repo, venue, record, :time_zone, :time_zone_last_updated_ticks)
+  end
+
+  defp get_record!(venue_uuid) do
     Records.Venue
     |> Venues.Query.snapshots_query()
     |> get_record!(Repo, venue_uuid)

6f93a17760f4cf18275320fadc7dc62b5c498189
Nicholas Henry <nicholas.henry@gmail.com>
2022-10-26 12:03:09 -0400

    refactor: Event publishing

--- apps/globo_ticket/lib/globo_ticket/promotions/acts/handlers/commands.ex
+++ apps/globo_ticket/lib/globo_ticket/promotions/acts/handlers/commands.ex
@@ -7,46 +7,52 @@ defmodule GloboTicket.Promotions.Acts.Handlers.Commands do
 
   alias GloboTicket.Promotions.Acts
   alias GloboTicket.Promotions.Acts.Messages
-  alias GloboTicket.Promotions.Acts.Projection
   alias GloboTicket.Promotions.Acts.Records
 
   def save_act(act) do
-    record = %Records.Act{uuid: act.id}
-
-    with {:ok, _} <- save_entity_record(Repo, record),
-         record = get_act!(act.id),
-         {:ok, _} <- save(Repo, act, record, :description) do
-      {:ok, act}
+    with {:ok, entity_record} <- save_entity(act) do
+      save_description(act, entity_record)
     end
   end
 
-  defp save(Repo, act, record, :description) do
-    with {:ok, description_record} <- save_snapshot(Repo, act, record, :description) do
-      applied_act = Projection.apply(act, description_record)
+  defp save_entity(act) do
+    entity_record = %Records.Act{uuid: act.id}
 
-      if Emu.Ticks.compare(applied_act.last_updated_ticks, act.last_updated_ticks) == :gt do
-        modified_date = Ticks.to_date_time(act.last_updated_ticks)
+    with {:ok, _entity_record} <- save_entity_record(Repo, entity_record) do
+      {:ok, get_record!(act)}
+    end
+  end
 
-        message = %Messages.Events.ActDescriptionChanged{
-          act_id: act.id,
-          act_description_representation: %Messages.Representations.ActDescription{
-            title: act.title,
-            image_hash: act.image,
-            modified_date: modified_date
-          }
-        }
+  defp save_description(act, entity_record) do
+    with {:ok, description_record} <- save_snapshot(Repo, act, entity_record, :description) do
+      last_updated_ticks = Ticks.from_date_time(description_record.inserted_at)
 
+      if Ticks.compare(last_updated_ticks, act.last_updated_ticks) == :gt do
+        message = build_act_description_changed(act, description_record)
         BusDriver.publish(:promotions, message)
       end
 
-      {:ok, applied_act}
+      {:ok, description_record}
     end
   end
 
-  defp get_act!(act_uuid) do
+  defp build_act_description_changed(act, description_record) do
+    act_description_representation = %Messages.Representations.ActDescription{
+      title: description_record.title,
+      image_hash: description_record.image,
+      modified_date: description_record.inserted_at
+    }
+
+    %Messages.Events.ActDescriptionChanged{
+      act_id: act.id,
+      act_description_representation: act_description_representation
+    }
+  end
+
+  defp get_record!(act) do
     Records.Act
     |> Acts.Query.snapshots_query()
-    |> get_record!(Repo, act_uuid)
+    |> get_record!(Repo, act.id)
   end
 
   def delete_act(act_uuid) do
